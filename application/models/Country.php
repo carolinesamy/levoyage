20379dafbe8b41b16ef0cb3f20188fe822f820fb
Caroline Samy <carolinsamy94@gmail.com>
2016-04-02 03:36:42 +0200

    start country page

--- application/models/Country.php
+++ application/models/Country.php
@@ -6,6 +6,13 @@ class Application_Model_Country extends Zend_Db_Table_Abstract
     //to add parent --dependent tables on it
     protected $_dependentTables=array('Application_Model_City');
 
+    function findCities($country_id)
+    {
+        $countryrow=$this->find($country_id)->current();
+        $cities=$countryrow->findDependentRowset('Application_Model_City');
+        return $cities;
+    }
+
 
 }
 
