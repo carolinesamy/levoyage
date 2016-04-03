f74bd167256ee80722289db45e3b8adf2deeb79b
aya <eng.aya136@gmail.com>
2016-04-03 19:08:25 +0200

    edit user profile

--- application/models/RentCar.php
+++ application/models/RentCar.php
@@ -1,8 +1,14 @@
 <?php
 
-class Application_Model_RentCar
+class Application_Model_RentCar extends Zend_Db_Table_Abstract
 {
+protected $_name = 'rent_car';
 
-
+protected $_referenceMap=array('user'=>array(
+        'columns'=>array('user_id'),
+        'refTableClass'=>'Application_Model_User',
+        'refColumns'=>array('id'),
+        'onDelete'=>'cascade'
+    ));
 }
 
